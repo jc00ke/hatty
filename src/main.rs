a33f68bcc7e290dbcbaba09c37590f849184105a
Jesse Cooke <jesse@relativepath.io>
2020-09-12 19:50:39 -0700

    Actually send the magic packet + tests

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use structopt::StructOpt;
 
 const MAC_SIZE: usize = 6;
 const MAC_PER_MAGIC: usize = 16;
-static HEADER: [u8; 6] = [0xFF; 6];
+const HEADER: [u8; 6] = [0xFF; 6];
+const PACKET_SIZE: usize = HEADER.len() + MAC_PER_MAGIC * MAC_SIZE;
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -33,18 +34,18 @@ struct Hatty {
 impl Hatty {
     pub fn send_magic_packet(&self) -> std::io::Result<()> {
         let socket = UdpSocket::bind("0.0.0.0:0")?;
-        match socket.send_to(b"hello", self.dest) {
-            Ok(nbytes) if nbytes == 5 => Ok(()),
+        match socket.send_to(self.build_magic_packet().as_slice(), self.dest) {
+            Ok(nbytes) if nbytes == PACKET_SIZE => Ok(()),
             Ok(nbytes) => Err(io::Error::new(
                 io::ErrorKind::Other,
-                format!("Expected 5 bytes, got {}", nbytes),
+                format!("Expected {} bytes, got {}", PACKET_SIZE, nbytes),
             )),
             Err(e) => Err(e),
         }
     }
 
     fn build_magic_packet(&self) -> Vec<u8> {
-        let mut packet = Vec::with_capacity(HEADER.len() + MAC_SIZE * MAC_PER_MAGIC);
+        let mut packet = Vec::with_capacity(PACKET_SIZE);
 
         let body: Vec<u8> = iter::repeat(self.mac.as_bytes())
             .take(MAC_PER_MAGIC)
@@ -80,24 +81,32 @@ mod tests {
 
     #[test]
     fn listen_for_magic_packet_test() -> std::io::Result<()> {
+        let mac_address: MacAddr6 = "18-C0-4D-42-2D-EA".parse().unwrap();
+        let mac_bytes: &[u8] = mac_address.as_bytes();
         let listen = "127.0.0.1:34356";
         let socket = UdpSocket::bind(listen)?;
         socket.set_read_timeout(Some(std::time::Duration::from_secs(1)))?;
 
-        let mut buf = [0; 102];
+        let mut buf = [0; PACKET_SIZE];
 
         let handle = std::thread::spawn(move || {
             let hatty = Hatty {
-                mac: "18-C0-4D-42-2D-EA".parse().unwrap(),
+                mac: mac_address,
                 dest: listen.parse().unwrap(),
             };
             hatty.send_magic_packet()
         });
 
         let (amt, _) = socket.recv_from(&mut buf)?;
+        assert_eq!(amt, PACKET_SIZE);
         handle.join().unwrap()?;
-        let rbuf = &buf[..amt];
-        assert_eq!(rbuf, b"hello");
+        let header = &buf[..HEADER.len()];
+        assert_eq!(header, vec![0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF].as_slice());
+
+        let body = &buf[HEADER.len()..];
+        for m in body.chunks(MAC_SIZE) {
+            assert_eq!(m, mac_bytes);
+        }
         Ok(())
     }
 
@@ -111,7 +120,7 @@ mod tests {
             dest: "127.0.0.1:7896".parse().unwrap(),
         };
         let magic_packet = hatty.build_magic_packet();
-        assert_eq!(magic_packet.len(), 102);
+        assert_eq!(magic_packet.len(), PACKET_SIZE);
         let (fs, macs) = magic_packet.split_at(MAC_SIZE);
         assert_eq!(fs, vec![255; MAC_SIZE].as_slice());
 
